15d6f4fe57b3d55ce5d2d48c3f729c3e2dcc9a18
Abhinav Bohra <abhinavbohra01@gmail.com>
2020-03-10 22:37:27 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener(function (request) {
+	var x = document.getElementsByClassName("WpHeLc")[0].href;
+	location.replace("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwik1uL_i5DoAhWzW3wKHVOiAlwQPAgH");
+ 	alert(x);
+ })
