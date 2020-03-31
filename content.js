fe14fceb593d514cb77b95b18b674d537a6498f3
Abhinav Bohra <abhinavbohra01@gmail.com>
2020-03-31 17:45:19 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,46 @@
-chrome.runtime.onMessage.addListener(function (request) {
-	var x = document.getElementsByClassName("WpHeLc")[0].href;
-	location.replace("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwik1uL_i5DoAhWzW3wKHVOiAlwQPAgH");
- 	alert(x);
- })
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+	//Step 1 : Check whether we have reached takeout page (logged in)
+	function checkURL(createExport)
+	{  
+		dateAtStartOfExecution = Date.now();
+		while(flag == 0)
+		{
+			if( window.location.href == "https://takeout.google.com/takeout")
+			{
+				flag=1;
+			}
+
+			if (Date.now() > dateAtStartOfExecution + 300000) //If takes moew than 5 minutes than we exit
+			{	
+				alert("Taking too much time....Killing");
+				return;
+			}
+		}
+	}
+	//Step 2: Create Export (Deselect All -> Select Voice activity -> Click on create export)
+	function createExport()
+	{
+		if(flag == 1)
+		{
+			var i;
+			for (i = 0; i < 44; i++)
+			{
+				document.getElementsByClassName("VfPpkd-muHVFf-bMcfAe")[i].click();	//Deselect all
+			}
+
+			document.getElementsByClassName("VfPpkd-muHVFf-bMcfAe")[32].click(); // Select 'Voice Activity'
+			document.getElementsByClassName("VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ nCP5yc AjY5Oe DuMIQc aMLfv")[0].click(); // Click on create export
+			//sendResponse();   //Somehow tell popup.js that its done
+		}
+
+		else
+		{
+			alert("Please login to your Google Account and then click on Create Export.")
+		}
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------------
+	//Execution starts from here
+	var flag=0;
+	checkURL(createExport);
+}
