be3285eae58c92e07e76a29db15033f46f524a4f
Abhinav Bohra <abhinavbohra01@gmail.com>
2020-03-31 17:46:18 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
-	document.querySelector('button').addEventListener('click',
-		onclick,false)
+	document.querySelector('button').addEventListener('click',onclick,false)
 
 	function onclick(){
-		chrome.tabs.query( {currentWindow: true, active: true} , function(tabs){chrome.tabs.sendMessage(tabs[0].id,'hi'); })
-
+		window.open("https://takeout.google.com/takeout/download"); //Loads Google Takeout Download Page
+		setTimeout(geURL,10000); //Waits for 10 seconds
+	
 	}
+	function getURL(){
+		chrome.tabs.query( {currentWindow: true, active: true} , function(tabs){chrome.tabs.sendMessage(tabs[0].id,'hi'); });
+	}
+
 }, false)
